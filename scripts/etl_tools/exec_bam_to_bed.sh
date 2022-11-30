7b57306aaac81cf9980c0b174720b713712e256b
yuohira <yuohira@ncc.go.jp>
2022-11-30 14:59:55 +0900

    add bam_to_bed script

---
+++ scripts/etl_tools/exec_bam_to_bed.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -o errexit
+set -o nounset
+set -o pipefail
+
+input_bam=${1}
+fasta_file=${2}
+threads=${3}
+
+file_name=`echo ${input_bam} | sed 's/\.[^\.]*$//'`
+output_bed="${file_name}.bed"
+
+/tools/modbam2bed/modbam2bed \
+  --aggregate \
+  --cpg \
+  -e \
+  -t ${threads} \
+  ${fasta_file} \
+  ${input_bam} > ${output_bed}
